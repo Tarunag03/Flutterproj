e0bd9dda720d6524c672b9a8a82c274d43a52f47
Vicky2114 <vr6994056@gmail.com>
2023-08-06 18:00:12 +0530

    8

--- chatapp/lib/main.dart
+++ chatapp/lib/main.dart
@@ -5,6 +5,6 @@ class Newone extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Text("fresh start newone");
+    return const Text("fresh nice start rice");
   }
 }
