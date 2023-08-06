b0f6f15981ef332a0d2f2688715ded63a91289ed
Tarunagr03 <Tarunagr03@gmail.com>
2023-08-06 17:39:59 +0530

    vickychange

--- chatapp/lib/main.dart
+++ chatapp/lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Just for the testing changes',
+      title: 'Hello vicky ji kya haal chal',
       theme: ThemeData(
         // This is the theme of your application.
         //
