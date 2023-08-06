c666ed7c86211bc2496a29ef2493d908de5d2202
Tarunagr03 <Tarunagr03@gmail.com>
2023-08-06 17:54:40 +0530

    newonefromtarun

--- chatapp/lib/main.dart
+++ chatapp/lib/main.dart
@@ -5,6 +5,6 @@ class Newone extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Text("fresh start something latest");
+    return const Text("fresh start something latest   dd ");
   }
 }
