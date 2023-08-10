b63186416038b1b81e74b94de3078b78f2d7e22a
Tarunagr03 <Tarunagr03@gmail.com>
2023-08-10 09:58:22 +0530

    tcd(phonelogin)

--- chatapp/lib/screens/PhoneAuthScreen.dart
+++ chatapp/lib/screens/PhoneAuthScreen.dart
@@ -1,6 +1,6 @@
 import 'dart:math';
 
-import 'package:chatapp/phone_auth/verify_otp.dart';
+
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
